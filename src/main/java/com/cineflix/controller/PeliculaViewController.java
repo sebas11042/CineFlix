4b543a699c1d8cf7556ea09b73c7c5aff4cdfca7
jonaikele27 <jonaikele27@mail.com>
2025-06-07 01:51:15 -0600

    DatallePelicula

--- src/main/java/com/cineflix/controller/PeliculaViewController.java
+++ src/main/java/com/cineflix/controller/PeliculaViewController.java
@@ -3,6 +3,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cineflix.entity.Pelicula;
 import com.cineflix.service.PeliculaService;
 
 @Controller
@@ -17,5 +20,15 @@ public class PeliculaViewController {
         model.addAttribute("peliculasOtras", peliculaService.obtenerPeliculasNoEstrenoNiProximas());
         model.addAttribute("peliculasProximas", peliculaService.obtenerPeliculasProximas());
         return "index";
+    }
+    @GetMapping("/pelicula/{id}")
+    public String verPeliculas(@PathVariable int id, Model model) {
+        Pelicula pelicula = peliculaService.findById(id);
+        if (pelicula != null) {
+            model.addAttribute("pelicula", pelicula);
+            return "DetallePelicula";
+        }
+        // Return a 404 error page if pelicula is not found
+        return "error/404";
     }
 }
