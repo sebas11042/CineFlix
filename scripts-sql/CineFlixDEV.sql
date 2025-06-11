70dfdb3a7a28662ed71904f6e2a51b8fdec2ae0d
Sebastián Gomez <sebastianga48@gmail.com>
2025-06-10 23:14:46 -0600

    script cineflixdev modified

--- scripts-sql/CineFlixDEV.sql
+++ scripts-sql/CineFlixDEV.sql
@@ -390,28 +390,42 @@ END;
 
 
 -- Este es el que va a hacer que cuando se paguen los boletos, esos asientos queden ocupados para futuras reservas
-CREATE OR ALTER PROCEDURE ocupar_asientos_funcion
+ALTER PROCEDURE [dbo].[ocupar_asientos_funcion]
     @idFuncion INT,
-    @asientos NVARCHAR(MAX) -- formato: '12,13,14'
+    @asientos NVARCHAR(MAX) -- Ej: '12,13,14'
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- Convertir la cadena CSV a XML para parsearla
-    DECLARE @xml XML = CAST('<root><id>' + 
-        REPLACE(@asientos, ',', '</id><id>') + 
+    DECLARE @xml XML = CAST('<root><id>' +
+        REPLACE(@asientos, ',', '</id><id>') +
         '</id></root>' AS XML);
 
-    -- Actualizar cada asiento en AsientoFuncion
+    -- Insertar si no existe
+    INSERT INTO AsientoFuncion (id_funcion, id_asiento, ocupado)
+    SELECT
+        @idFuncion,
+        x.value('.', 'INT'),
+        1
+    FROM @xml.nodes('/root/id') AS t(x)
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM AsientoFuncion af
+        WHERE af.id_funcion = @idFuncion
+          AND af.id_asiento = x.value('.', 'INT')
+    );
+
+    -- Por si ya existían y estaban en false, asegurarse de marcarlos ocupados
     UPDATE af
     SET af.ocupado = 1
     FROM AsientoFuncion af
     INNER JOIN (
         SELECT x.value('.', 'INT') AS id_asiento
         FROM @xml.nodes('/root/id') AS t(x)
-    ) AS parsed ON af.id_asiento = parsed.id_asiento
+    ) parsed ON af.id_asiento = parsed.id_asiento
     WHERE af.id_funcion = @idFuncion;
 END;
+
 
 
 
@@ -422,3 +436,9 @@ FROM sys.triggers
 WHERE parent_id = OBJECT_ID('dbo.Pago');
 
 DISABLE TRIGGER trg_no_pago_invalido ON dbo.Pago;
+
+
+SELECT TOP 10 * FROM Boleto ORDER BY id_boleto DESC;
+
+
+SELECT * FROM AsientoFuncion WHERE id_funcion = 5;
